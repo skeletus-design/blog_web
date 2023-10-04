50665d861c0660fb81d2d66961140add122bcad2
skeletus-design <nikita.voro24nin@gmail.com>
2023-10-04 15:28:38 +0300

    done_superuser

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+
 
 class AboutView(TemplateView):
     template_name = "main.html"
@@ -17,3 +20,24 @@ class profile(TemplateView):
 def registration(request):
     if request.method == "POST":
         return render(request, 'registration.html')
+
+# def SignIn(forms.Form):
+#     if request.method == 'POST':
+#         form = SignIn        
+#             username = forms.CharField(max_length=16)
+#             password = forms.PasswordInput()
+
+# Логин
+def login_(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        
+# if request.user.is_authenticated:
+#         return render(request, 'index.html') 
+#     else:
+#         return redirect('/sign_in')
