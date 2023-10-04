8ef1b318d90c82f77039a0b9761d259ca42abee2
TommyHellANiggets <112550948+TommyHellANiggets@users.noreply.github.com>
2023-10-04 21:27:24 +0300

    big penis

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("auth/", views.auth.as_view(), name="auth"),
     path("registration_page/", views.registration_page.as_view(), name="registration_page"),
     path("profile/", views.profile.as_view(), name="profile"),
-    path("", views.registration, name="registr")
+    path("", views.registration, name="registr"),
+    path("add_post/", views.add_post.as_view(), name="add_post"),
 ]
 
