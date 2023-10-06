3c5066383e0a2bba1bc07aafc0a4c0c2969c7bfa
skeletus-design <nikita.voro24nin@gmail.com>
2023-10-07 00:01:31 +0300

    bb

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from polls.forms import RegistrationFrom
+from django.contrib.auth.forms import UserCreationForm
+# from polls.forms import RegistrationFrom
 from django.contrib.auth import authenticate, login, logout
-from django_registration.backends.one_step.views import RegistrationView
+# from django_registration.backends.one_step.views import RegistrationView
 
+def index(request):
+    return render(request, 'main.html')
 
+def registration(request):
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+        return redirect('index')
+    else:
+        user_form = UserCreationForm()
+    return render(request, 'registration.html', {'user_form': user_form})
 
 
 # class AboutView(TemplateView):                               
@@ -26,11 +38,15 @@ class registration_page(TemplateView):
 class profile(TemplateView):
     template_name = "profile.html"
     
-    
+def log_out(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/auth')   
+     
 
-# def SignIn(forms.Form):
+# def SignIn(forms.user_f):
 #     if request.method == 'POST':
-#         form = SignIn        
+#         form = SignIn
 #             username = forms.CharField(max_length=16)
 #             password = forms.PasswordInput()
 
@@ -46,18 +62,18 @@ def login_(request):
         else:
             return redirect('auth')
 
-def sign_up(request):
-    if request.method == 'POST':
-        print('bigballs')
-        form = RegistrationFrom()
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('auth')
-        return render(request, 'registration.html', { 'form': form})
-    else:
-        form = RegistrationFrom()
-        return render(request, 'registration.html', { 'form': form})ы
+# def sign_up(request):
+#     if request.method == 'POST':
+#         print('bigballs')
+#         form = RegistrationFrom()
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('auth')
+#         return render(request, 'registration.html', { 'form': form})
+#     else:
+#         form = RegistrationFrom()
+#         return render(request, 'registration.html', { 'form': form})
     
     
 # def sign_upp(request):
@@ -66,28 +82,27 @@ def sign_up(request):
 #         if form.is_valid():
 #             user = form.save()
 #             login(request, user)
-#             return redirect('auth')
+#             return redirect('autdef
 
 
-class CustomRegistrationView(RegistrationView):
-    """
-    Вьюшка для регистрации пользователя в аккаунт
-    """
-
-    template_name = 'registration.html'
-    form_class = RegFrom
-    success_url = reverse_lazy('profile')
-
-    def dispatch(self, args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect(self.success_url)
-
-        return super().dispatch(args, **kwargs)
+# class CustomRegistrationView(RegistrationView):
+#     """
+#     Вьюшка для регистрации пользователя в аккаунт
+#     """
+#
+#     template_name = 'registration.html'
+#     form_class = RegFrom
+#     success_url = reverse_lazy('profile')
+#
+#     def dispatch(self, args, **kwargs):
+#         if self.request.user.is_authenticated:
+#             return redirect(self.success_url)
+#
+#         return super().dispatch(args, **kwargs)
         
 
-def index(request):
-    if request.user.is_authenticated:
-        return render(request, 'main.html') 
-    else:
-        return redirect('/auth')
-
+# def index(request):
+#     if request.user.is_authenticated:
+#         return render(request, 'main.html')
+#     else:
+#         return redirect('/auth')
